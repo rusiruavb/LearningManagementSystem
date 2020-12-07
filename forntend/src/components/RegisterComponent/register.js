5bdd6e3b6df6f48fd4c210f493815d5aef587f85
SenuraJayadeva <senurajayadeva@gmail.com>
2020-12-07 12:14:18 +0530

    installed material ui

--- forntend/src/components/RegisterComponent/register.js
+++ forntend/src/components/RegisterComponent/register.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import "./register.css";
+
 export default function RegisterComponent() {
   return (
     <div className="RegisterBody">
-      <div className="row"></div>
+      <div className="row">
+        <div className="col-md-6">
+          <h3>Learning Management</h3>
+        </div>
+
+        <div className="col-md-6">
+          <h3>Learning Management</h3>
+        </div>
+      </div>
     </div>
   );
 }
