510f1fbd5186481719f358815aa791cd144a8a03
Rusiru Abhisheak <rusiruavb98@gmail.com>
2020-12-07 11:55:35 +0530

    create login component

--- forntend/src/App.js
+++ forntend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Login from './components/LoginComponent/login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Leaning Management App</h1>
-
+      <Login/>
     </div>
   );
 }
---
+++ forntend/src/components/LoginComponent/login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './login.css'
+
+const Login = () => {
+  return (
+    <div className="LoginBody">
+      <div className="row">
+        <div className="col-6"></div>
+        <div className="col-6">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
